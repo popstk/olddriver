69bdad670323f4dcff438100618d45e4e44eace1
popstk <ther@live.cn>
2018-01-19 10:21:48 +0800

    add dupan re

--- crawler/spiders/Hacg.py
+++ crawler/spiders/Hacg.py
@@ -5,19 +5,19 @@ import re
 
 def get_magnet_links(result_text):
     result_text = re.sub(r'<[\s\S]*?>', '', result_text)
-    result_text = re.sub(
-        r'([^0-9a-zA-Z])([0-9a-zA-Z]{5,30})[^0-9a-zA-Z]{5,30}([0-9a-zA-Z]{5,30})([^0-9a-zA-Z])', r'\1\2\3\4', result_text)
+    # 可能是截断的，先拼起来
+    result_text = re.sub(r'([^0-9a-zA-Z])([0-9a-zA-Z]{5,30})[^0-9a-zA-Z]{5,30}([0-9a-zA-Z]{5,30})([^0-9a-zA-Z])', r'\1\2\3\4', result_text)
 
-    hashes = list(
-        set(re.findall(r'[^0-9a-fA-F]([0-9a-fA-F]{40})[^0-9a-fA-F]', result_text)))
-    hashes.extend(
-        list(set(re.findall(r'[^0-9a-zA-Z]([0-9a-zA-Z]{32})[^0-9a-zA-Z]', result_text))))
-    magnets = list(set([hash_value.lower() for hash_value in hashes]))
-    return magnets
+    # 40位和32位的磁力链接
+    hashes = list(set(re.findall(r'[^0-9a-fA-F]([0-9a-fA-F]{40})[^0-9a-fA-F]', result_text)))
+    hashes.extend(list(set(re.findall(r'[^0-9a-zA-Z]([0-9a-zA-Z]{32})[^0-9a-zA-Z]', result_text))))
+    return = list(set([hash_value.lower() for hash_value in hashes]))
+
 
 
 def get_dupan_links(result_text):
-    return []
+    pairs = re.findall(r'\W(\w{8})\W*(\w{4})\W', result_text)
+    return = list(set(p[0] + '#' + p[1] for p in pairs))
 
 
 class HacgSpider(scrapy.Spider):
