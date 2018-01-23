9786c439baf41ac297a05b7bfa9607c7362569d6
popstk <ther@live.cn>
2018-01-23 14:57:08 +0800

    ignore sth

--- tool/convert2mongo.py
+++ tool/convert2mongo.py
@@ -1,7 +1,10 @@
 import pymongo
 import json
 
+# wget
+# https://raw.githubusercontent.com/Chion82/hello-old-driver/master/resource_list.json
+
 client = pymongo.MongoClient()
 db = client['spider']
-v = json.loads(open('hacg_backup.json').read().decode('utf8'))
+v = json.loads(open('resource_list.json').read().decode('utf8'))
 db.llss.insert_many(v)
