3a39b5b5b8c3c16c42ec2243efc146caab777fe7
popstk <ther@live.cn>
2018-01-31 23:54:35 +0800

    remove sth

--- backend/resources/spider.py
+++ backend/resources/spider.py
@@ -4,25 +4,34 @@ from flask_restful import Resource
 from scrapyd_api import ScrapydAPI
 
 
+def listjobs(project, name):
+    for status in ('running', 'finished', 'pending'):
+        for job in project[status]:
+            job['status'] = status
+            job['spider'] = name + '.' + job['spider']
+            yield job
+
+
+def reduce_spiders(jobs):
+    spiders = {}
+    for job in jobs:
+        if job['spider'] not in spiders:
+            spiders[job['spider']] = [job]
+        else:
+            spiders[job['spider']].append(job)
+    return [reduce(lambda x, y: x if x['start_time'] > y['start_time'] else y, v) for v in spiders.itervalues()]
+
+
 class Spider(Resource):
     def __init__(self, url):
         self.scrapyd = ScrapydAPI(url)
 
     def get(self):
-        data = {}
+        data = []
         for p in self.scrapyd.list_projects():
-            jobs = self.scrapyd.list_jobs(p)
-            for status in ('running', 'finished', 'pending'):
-                for job in jobs[status]:
-                    spider = "%s.%s" % (p, job.pop('spider'))
-                    job['status'] = status
-                    if spider not in data:
-                        data[spider] = {'jobs': [],
-                                        'running': False, 'spider': spider}
-                    data[spider]['jobs'].append(job)
-                    if status == 'running' or status == 'pending':
-                        data[spider]['running'] = True
-        return data.values()
+            spiders = reduce_spiders(listjobs(self.scrapyd.list_jobs(p), p))
+            data.extend(spiders)
+        return data
 
     def post(self, name):
         p, s = name.split('.')
@@ -31,3 +40,8 @@ class Spider(Resource):
             if job['spider'] == s:
                 return 'Already Running'
         return self.scrapyd.schedule(p, s)
+
+
+if __name__ == '__main__':
+    s = Spider('http://45.76.111.18:6800')
+    print(s.get())
